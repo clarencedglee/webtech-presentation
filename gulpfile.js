7022b1b210c55249f8fd5e9d4aa496e123b6ea5d
chubishere <clarence@dubishere.com>
2017-05-04 18:13:46 +0100

    adding animation

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,22 @@ let gulp = require('gulp')
 let insert = require('gulp-insert')
 let markdown = require('gulp-markdown')
 let livereload = require('gulp-livereload')
+let sass = require('gulp-sass')
 let fs = require('fs')
 let livereloadPort = 9124
 
-gulp.task('default', ['simple'], () => {
+let scss = ['*.scss']
+
+gulp.task('default', ['html'], () => {
   livereload.listen({port: livereloadPort})
-  gulp.watch(['./index.html', './style.css', './script.js', './defaults/*.*', './out/dev.css'], ['simple'])
+  gulp.watch(['./index.html', './style.css', './script.js', './defaults/*.*', './out/dev.css'], ['html'])
+  gulp.watch(scss, ['css']);
 })
 
-gulp.task('simple', () => {
+gulp.task('html', () => {
   let style = fs.readFileSync('style.css', 'utf8')
   let script = fs.readFileSync('script.js', 'utf8')
-  gulp.src('./index.html')
+  return gulp.src('./index.html')
       .pipe(insert.transform((contents, file) => {
         return contents.replace(/\b([-.\w]+)\.j\b/gm, '<img src="$1.jpg" width="100%"/>')
       }))
@@ -28,27 +32,12 @@ gulp.task('simple', () => {
       .pipe(livereload())
 })
 
-gulp.task('dubpage', () => {
-  let defaultStyles = fs.readFileSync('defaults/style.css', 'utf8')
-  let script = fs.readFileSync('defaults/script.js', 'utf8')
-  gulp.src('./index.html')
-      .pipe(insert.transform((contents, file) => {
-        return contents.replace(/\b([-.\w]+)\.j\b/gm, '<img src="$1.jpg" />')
-      }))
-      .pipe(markdown())
-      .pipe(insert.append(emptyLines(30)))
-      // .pipe(insert.append(`<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">`))
-      .pipe(insert.append(`<script src="http://localhost:9123/livereload.js?snipver=1"></script>`))
-      .pipe(insert.append(`<style>${defaultStyles}</style>`))
-      .pipe(insert.append(`<script>${script}</script>`))
-      .pipe(gulp.dest('./out'))
-      .pipe(livereload())
+gulp.task('css', () => {
+  return gulp.src(scss)
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./out'))
+		.pipe(livereload())
 })
-
-function emptyLines (n) {
-  return [...new Array(n)].reduce(a => a + '\n', '')
-}
-
 
 function emptyLines (n) {
   return [...new Array(n)].reduce(a => a + '\n', '')
